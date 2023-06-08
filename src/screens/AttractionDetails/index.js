edc678b5deadd621077effe128f9812bf462c35b
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-06-08 10:50:15 +0330

    6. Handling Back Events

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 import styles from './styles';
 
-const  AttractionDetails = ({route}) => {
+const  AttractionDetails = ({navigation,route}) => {
     const {item} = route?.params || {};
+    const onBack = () =>{
+        navigation.goBack();
+    }
     return(
         <SafeAreaView>
+            <Text onPress={onBack} style={{margin:32}}>Back</Text>
                  <Text>{item?.name}</Text>
         </SafeAreaView>
     )
