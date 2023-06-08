09c169cc6f8ed79f18d3f91c969ebc7ac8acd31f
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-06-08 11:27:49 +0330

    1. Using ImageBackground

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { ImageBackground,SafeAreaView, Text } from "react-native";
 import styles from './styles';
 
 const  AttractionDetails = ({navigation,route}) => {
     const {item} = route?.params || {};
+    const mainImage = item?.images?.length ? item?.images[0] : null;
     const onBack = () =>{
         navigation.goBack();
     }
     return(
-        <SafeAreaView>
-            <Text onPress={onBack} style={{margin:32}}>Back</Text>
+        <SafeAreaView style={styles.container}>
+             <ImageBackground style={styles.mainImage} imageStyle={{ borderRadius: 20 }} source={{ uri: mainImage }} />
+              <Text onPress={onBack} style={{margin:32}}>Back</Text>
                  <Text>{item?.name}</Text>
         </SafeAreaView>
     )
