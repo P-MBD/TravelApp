25a389506b7ae7b5425430f1d38e40ce224a796e
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-06-10 09:25:02 +0330

    2. Adding Actions inside ImageBackground

--- src/screens/AttractionDetails/index.js
+++ src/screens/AttractionDetails/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground,SafeAreaView, Text } from "react-native";
+import { Image, ImageBackground, Pressable, SafeAreaView, Text, View } from 'react-native';
 import styles from './styles';
 
 const  AttractionDetails = ({navigation,route}) => {
@@ -10,9 +10,33 @@ const  AttractionDetails = ({navigation,route}) => {
     }
     return(
         <SafeAreaView style={styles.container}>
-             <ImageBackground style={styles.mainImage} imageStyle={{ borderRadius: 20 }} source={{ uri: mainImage }} />
-              <Text onPress={onBack} style={{margin:32}}>Back</Text>
-                 <Text>{item?.name}</Text>
+             <ImageBackground style={styles.mainImage} 
+             imageStyle={{ borderRadius: 20 }} 
+             source={{ uri: mainImage }} >
+                     <View style={styles.header}>
+                <Pressable onPress={onBack} hitSlop={8}>
+                     <Image style={styles.icon} source={require('../../assets/back.png')} />
+                </Pressable>
+                
+                <Pressable hitSlop={8}>
+                       <Image style={styles.icon} source={require('../../assets/share.png')} />
+                </Pressable>
+                    
+              </View>
+
+              <View style={styles.footer}>
+                  {item?.images?.length ? item?.images?.map(image => (
+                    <Image key={image} source={{uri:image}} style={styles.miniImage} />
+                  )): null}
+              </View>
+         
+              </ImageBackground>
+
+              <View>
+                  <Text>{item?.name}</Text>
+              </View>
+         
+              
         </SafeAreaView>
     )
 }
--- src/screens/AttractionDetails/styles.js
+++ src/screens/AttractionDetails/styles.js
@@ -9,7 +9,37 @@ const styles = StyleSheet.create({
     mainImage:{
         width:'100%',
         height:height/2,
+        flexDirection:'column',
+        justifyContent:'space-between',
     },
+    header:{
+        width:'100%',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+    },
+    icon:{
+        width: 36,
+        height: 36,
+        margin:16,
+    },
+    footer:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:10,
+        backgroundColor:'rgba(256, 256, 256, 0.35)',
+        margin:16,
+        paddingHorizontal:8,
+    },
+    miniImage:{
+        width: 40,
+        height: 40,
+        margin:8,
+        borderRadius:10,
+        backgroundColor:'rgba(256, 256, 256, 0.35)',
+    },
+
 });
 
 export default styles;
